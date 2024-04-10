90e376aa49af6e99020d39d5cbbe294982c7cf94
oskarc35 <labourers_aslinux@aleeas.com>
2024-04-10 16:26:40 +0200

    test

--- src/app.controller.spec.ts
+++ src/app.controller.spec.ts
@@ -13,4 +13,10 @@ describe('AppController', () => {
 
     appController = app.get<AppController>(AppController);
   });
+
+  describe('root', () => {
+    it('should return "Hello World!"', () => {
+      expect(true).toBe(true);
+    });
+  });
 });
